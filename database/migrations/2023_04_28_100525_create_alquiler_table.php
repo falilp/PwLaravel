fa85fe5eee24c5367a624da9565d8e814cfd1a52
falilp <falilealp@gmail.com>
2023-04-28 12:13:57 +0200

    alquiler migration

---
+++ database/migrations/2023_04_28_100525_create_alquiler_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void{
+        Schema::create('alquiler', function (Blueprint $table){
+            //$table->id();
+            $table->increments('codPista');
+            $table->integer('codUsuario')->unsigned();
+            $table->dateTime('fecha_alquiler');
+            $table->smallInteger('precio',false);
+            $table->float('descuento');
+            $table->foreign('codPista')->references('codPista')->on('pista');
+            $table->foreign('codUsuario')->references('codUsuario')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void{
+        Schema::dropIfExists('alquiler');
+    }
+};
